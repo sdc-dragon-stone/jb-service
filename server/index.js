bb147202ffe4d879fd38a05ea7e4eca07a489166
Mike <mikeglopez.91@gmail.com>
2019-05-01 23:26:28 -0500

    Added gzip back into orderPreference

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', expressStaticGzip(`${__dirname}/../public`, {
   index: false,
   enableBrotli: true,
-  orderPreference: ['br'],
+  orderPreference: ['br', 'gz'],
   setHeaders: (res) => {
     res.setHeader('Cache-Control', 'public, max-age=31536000');
   }
