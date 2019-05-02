431be9a84a920318670fa0c6d71a656f40f3b3f4
Mike <mikeglopez.91@gmail.com>
2019-05-01 21:14:07 -0500

    Fix text-compression

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const db = require('../database/index.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(expressStaticGzip('/../public', {
+app.use('/', expressStaticGzip(`${__dirname}/../public`, {
   index: false,
   enableBrotli: true,
   orderPreference: ['br', 'gz'],
