0ec7c5b3b76307eaa47bb9dce0c0f42820b12810
Jesse Bell <jkurtasbell@gmail.com>
2019-05-08 22:59:30 -0700

    Add createOne.js to create a single item for post endpoint

---
+++ database/createOne.js
@@ -0,0 +1,29 @@
+const faker = require('faker');
+const generator = require('../helpers/generator.js');
+
+const item = () => {
+  const oneItem = [];
+  for (let i = 0; i < 1; i++) {
+    const noun = generator.genNoun();
+    const numBedrooms = generator.genNumBedrooms(noun);
+    const numGuests = generator.genNumGuests();
+    oneItem.push({
+      host: {
+        name: faker.name.firstName(),
+        pic: faker.image.avatar()
+      },
+      title: generator.genTitle(noun),
+      city: generator.genCity(),
+      numGuests,
+      homeType: generator.genHomeType(noun),
+      numBedrooms,
+      numBeds: generator.genNumBeds(numGuests),
+      numBaths: generator.genNumBaths(),
+      description: generator.genDescription(noun, numBedrooms)
+    });
+  }
+  console.log('oneItem', oneItem);
+  return oneItem;
+};
+
+module.exports = { item };
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const expressStaticGzip = require('express-static-gzip');
 const port = process.env.PORT || 3210;
 const app = express();
 const db = require('../database/index.js');
+const createOne = require('../database/createOne.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,18 +32,26 @@ app.get('/description', (req, res) => {
 });
 
 app.post('/post', (req, res) => {
-
+  console.log('inside post')
+  let oneItem = createOne.item();
+  db.save({oneItem}, (err, result) => {
+    if (err) { throw err; }
+    console.log('saved', result);
+  });
 });
 
 app.delete('/delete', (req, res) => {
-  db.deleteOne({}, (err) => {
+  db.deleteOne({}, (err) => { // not sure what to pass into db.deleteOne
     if (err) { throw err; }
     console.log('item deleted!');
   });
 });
 
 app.put('/put', (req, res) => {
-  db.findByIdAndUpdate(id, update, options, callback)
+  db.findByIdAndUpdate(id, update, (err, result) => {
+    if (err) { throw err; }
+    console.log('updated', result);
+  });
 });
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
