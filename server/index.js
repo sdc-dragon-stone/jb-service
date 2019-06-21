0be96603923233d3cdfa45bd16e45af74ae3d6da
Jesse Bell <jkurtasbell@gmail.com>
2019-06-21 12:47:29 -0400

    Add headers for cross-origin in server/index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,11 @@ app.use('/', expressStaticGzip(`${__dirname}/../public`, {
 }));
 */
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(express.static('./public'));
 app.use('/:id', express.static('./public'));
